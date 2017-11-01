e43bc6893512af06273898eb61c985ac495a7380
kosta709 <kosta@codefresh.io>
2017-11-01 14:51:09 +0200

    Added KUBECONTEXT as a mandatory variable along with NAMESPACE fix

--- cf-deploy-kubernetes.sh
+++ cf-deploy-kubernetes.sh
@@ -5,35 +5,38 @@ fatal() {
    exit 1
 }
 
-readonly DEFAULT_NAMESPACE=${KUBERNETES_NAMESPACE:-default}
 readonly KUBECTL_ACTION=${KUBECTL_ACTION:-apply}
 [[ $KUBECTL_ACTION =~ ^(apply|create|replace)$ ]] || fatal "KUBECTL_ACTION should be one of apply|create|replace "
 
 deployment_file=${1:-deployment.yml}
-
+: ${KUBERNETES_NAMESPACE:-default}
 : ${KUBERNETES_DEPLOYMENT_TIMEOUT:=120}
 
 
-if [[ -z "${KUBECONFIG}" ]]; then
-
-    [ -z "$KUBERNETES_USER" ] && echo "Please set KUBERNETES_USER" && exit 1;
-    [ -z "$KUBERNETES_PASSWORD" ] && echo "Please set KUBERNETES_PASSWORD" && exit 1;
-    [ -z "$KUBERNETES_SERVER" ] && echo "Please set KUBERNETES_SERVER" && exit 1;
+if [[ -n "$KUBERNETES_SERVER" && -n "$KUBERNETES_USER" && -n "$KUBERNETES_PASSWORD" ]]; then
+    unset KUBECONFIG
 
     echo "---> Setting up Kubernetes credentials..."
     kubectl config set-credentials deployer --username=$KUBERNETES_USER --password=$KUBERNETES_PASSWORD
     kubectl config set-cluster foo.kubernetes.com --insecure-skip-tls-verify=true --server=$KUBERNETES_SERVER
-    kubectl config set-context foo.kubernetes.com/deployer --user=deployer --namespace=$DEFAULT_NAMESPACE --cluster=foo.kubernetes.com
+    kubectl config set-context foo.kubernetes.com/deployer --user=deployer --namespace=$KUBERNETES_NAMESPACE --cluster=foo.kubernetes.com
     kubectl config use-context foo.kubernetes.com/deployer
+
+    KUBECONTEXT=foo.kubernetes.com/deployer
+else
+    if [[ -z "${KUBECONTEXT}" ]]; then
+        # If KUBECONFIG is set we obligate to set KUBECONTEXT to valid context name
+        if [[ -n "${KUBECONFIG}" ]]; then
+          echo -e "--- ERROR - KUBECONTEXT Environment variable is not set, please set it to one of integrated contexts: "
+          kubectl config get-contexts
+          fatal "KUBECONTEXT is not set "
+        else
+           KUBECONTEXT=$(kubectl config current-context)
+        fi
+    fi
 fi
 
-if [[ -n KUBECONTEXT ]]; then
-  kubectl config use-context "${KUBECONTEXT}"
-fi
-
-# [ -z "$DOCKER_IMAGE_TAG" ] && echo "Please set DOCKER_IMAGE_TAG" && exit 1;
 [ ! -f "${deployment_file}" ] && echo "Couldn't find $deployment_file file at $(pwd)" && exit 1;
-
 
 DEPLOYMENT_FILE=${deployment_file}-$(date '+%y-%m-%d_%H-%M-%S').yml
 $(dirname $0)/template.sh "$deployment_file" > "$DEPLOYMENT_FILE" || fatal "Failed to apply deployment template on $deployment_file"
@@ -49,11 +52,11 @@ fi
 
 DEPLOYMENT_NAME=$(awk '/^Deployment /{a=$2}END{print a}' $KUBECTL_OBJECTS)
 
-echo "---> Submitting a deployment to Kubernetes by kubectl $KUBECTL_ACTION "
-kubectl $KUBECTL_ACTION -f "$DEPLOYMENT_FILE" || fatal "Deployment submitting Failed"
+echo "---> Submitting a deployment to Kubernetes by
+   kubectl --context "${KUBECONTEXT}" --namespace "${KUBERNETES_NAMESPACE}" $KUBECTL_ACTION "
+kubectl --context "${KUBECONTEXT}" --namespace "${KUBERNETES_NAMESPACE}" $KUBECTL_ACTION -f "$DEPLOYMENT_FILE" || fatal "Deployment submitting Failed"
 
 if [ -n "$DEPLOYMENT_NAME" ]; then
-    echo "---> Waiting for a successful deployment/${DEPLOYMENT_NAME} status..."
-    timeout -s SIGTERM -t $KUBERNETES_DEPLOYMENT_TIMEOUT kubectl rollout status deployment/"${DEPLOYMENT_NAME}" || fatal "Deployment Failed"
+    echo "---> Waiting for a successful deployment/${DEPLOYMENT_NAME} status to namespace ${KUBERNETES_NAMESPACE} ..."
+    timeout -s SIGTERM -t $KUBERNETES_DEPLOYMENT_TIMEOUT kubectl --context "${KUBECONTEXT}" --namespace "${KUBERNETES_NAMESPACE}" rollout status deployment/"${DEPLOYMENT_NAME}" || fatal "Deployment Failed"
 fi
-
